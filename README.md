d8ef801c956db97858316a87e1088579d7d5bbea
kevinbird61 <kevinbird61@gmail.com>
2018-01-22 00:48:02 +0800

    0.1.0
    * Support dynamic generating meme!
    * Using a json file as configuration.

--- README.md
+++ README.md
@@ -13,11 +13,31 @@ npm install memegen -g
 
 ## Usage
 
+### Static Template (For Drake meme)
 * Example:
 
 ```bash
 memegen -1 upper.png -2 lower.png -o output_dir -f output_filename -t png
 ```
+
+### Dynamic Configuration
+* [Config file format](https://github.com/toolbuddy/memegen/wiki/Configuration)
+
+* Example:
+
+```bash
+memegen -c <config>.json -f <output_file_name> -t png -o <output_dir>
+```
+
+* See some example, which generate from specific configuration file!
+    * Cereal guy meme
+        * [configure file](config_cereal.json)
+        * `memegen -c config_cereal.json -f use_config_cereal -t png -o output`
+        ![](output/use_config_cereal.png)
+    * Peter parker meme
+        * [configure file](config.json)
+        * `memegen -c config.json -f use_config -t png -o output`
+        ![](output/use_config.png)
 
 * or just using `--help`:
 
--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 const sharp = require('sharp');
 const chalk = require('chalk');
 const program = require('commander');
+const jsfs = require('jsonfile');
 const fsx = require('fs-extra')
 const fs = require('fs');
 const path = require('path');
@@ -14,33 +15,43 @@ program
   .description('A meme generator.')
   .option('-1, --first [name]', 'Specify first input image(upper one)')
   .option('-2, --second [name]', 'Specify second input image(lower one)')
+  .option('-c, --config [name]', 'Specify configure file for usage',undefined)
   .option('-o, --output [loc]', 'Specify output location',__dirname)
   .option('-f, --fname [name]', 'Specify output filename','output')
-  .option('-type, --type [type]', 'Specify output file type','.png')
+  .option('-t, --type [type]', 'Specify output file type','png')
   .parse(process.argv);
 
-// Testing all file is legal
-fsx.pathExists(program.first, (err,exists) => {
-  if(exists==false){
-    console.error(`${program.first} is not existed!`);
-    process.exit();
-  }
-  else{
-    console.log(`Upper image: ${program.first}`);
-    // Checking second 
-    fsx.pathExists(program.second, (err,exists) => {
-      if(exists==false){
-        console.error(`${program.second} is not existed!`);
-        process.exit();
-      }
-      else{
-        console.log(`Lower image: ${program.second}`);
-      }
-    })
-    // calling generating function
-    generate_meme(program.first,program.second,program.output);
-  }
-});
+if(program.config != undefined){
+  // use configuration files
+  let jsobj = jsfs.readFileSync(program.config,'utf-8');
+  // 
+  generate_meme_advanced(jsobj);
+}
+else{
+  // use drake as template
+  // Testing all file is legal
+  fsx.pathExists(program.first, (err,exists) => {
+    if(exists==false){
+      console.error(`${program.first} is not existed!`);
+      process.exit();
+    }
+    else{
+      console.log(`Upper image: ${program.first}`);
+      // Checking second 
+      fsx.pathExists(program.second, (err,exists) => {
+        if(exists==false){
+          console.error(`${program.second} is not existed!`);
+          process.exit();
+        }
+        else{
+          console.log(`Lower image: ${program.second}`);
+        }
+      })
+      // calling generating function
+      generate_meme(program.first,program.second,program.output);
+    }
+  });
+}
 
 
 // Generate meme!
@@ -60,10 +71,40 @@ function generate_meme(src1,src2,out){
             .toBuffer(function(err,result1) {
               sharp(result1)
                 .overlayWith(out2, { top:265, left: 322 } )
-                .toFile(path.join(program.output,program.fname+program.type), function(err) {
+                .toFile(path.join(program.output,program.fname+"."+program.type), function(err) {
                 });
             });
           
         })
     })
+}
+
+function generate_meme_advanced(configobj){
+  let count=0;
+  configobj.input.forEach(obj => {
+    //console.log(obj)
+    //console.log(configobj.bg)
+    sharp(obj.src)
+      .resize(obj.size.w,obj.size.h)
+      .max()
+      .toBuffer(function(err,out1){
+        // overlaywith process
+        console.log(count);
+        if(count==0){
+          sharp(configobj.bg.src)
+            .overlayWith(out1, { top:obj.loc.top , left: obj.loc.left } )
+            .toFile('/tmp/test.png', function(err) {
+            });
+        }
+        else{
+          setTimeout(function(){
+            sharp('/tmp/test.png')
+              .overlayWith(out1, { top:obj.loc.top , left: obj.loc.left } )
+              .toFile(path.join(program.output,program.fname+"."+program.type), function(err) {
+              });
+          },100)
+        }
+        count++;
+    })
+  })
 }
